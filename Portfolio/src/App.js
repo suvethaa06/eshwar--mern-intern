e2bbd67e8f3344cee2954f6cbbad0256897cc887
suvethaa06 <155346431+suvethaa06@users.noreply.github.com>
2025-01-29 10:30:06 +0530

    Add files via upload

---
+++ Portfolio/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Port from "./port.js"
+
+function App() {
+  return (
+    <div className="App">
+      <Port />
+    </div>
+  );
+}
+
+export default App;
+                                                                                                                                                                                                                                                                                                                                                                   
---
+++ Portfolio/src/port.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+function Port(){
+    return(
+     
+        <body>
+  <header>
+    <h1>Suvethaa D R</h1>
+   <p>Phone: +91 6379054448 | Email: suvethaa.d2023aiml@sece.ac.in</p>
+  </header>  
+  <nav>
+    <ul>
+      <li><a href="#education">Education</a></li>
+      <li><a href="#projects">Projects</a></li>
+      <li><a href="#skills">Skills</a></li>
+      <li><a href="#certifications">Certifications</a></li>
+      <li><a href="#achievements">Achievements</a></li>
+    </ul>
+  </nav>
+
+  <section id="education">
+    <h2>Education</h2>
+    <ul>
+      <li><b>Sri Eshwar College of Engineering:</b> B.E. CSE (AIML) – CGPA: 8.05 (2023–2027)</li>
+      <li><b>Senthil Matric Hr. Sec. School:</b> HSC – 90% (2021–2023)</li>
+      <li><b>SSLC:</b> All Pass (2020–2021)</li>
+    </ul> 
+  </section>
+
+  <section id="projects">
+    <h2>Projects</h2>
+    <ul>
+      <li><b>Brain Tumor Prediction:</b> MRI classification using TensorFlow, CNN, and NumPy.</li>
+      <li><b>Sign Language Converter:</b> Real-time hand gesture translation using CNNs.</li>
+      <li><b>Bank Management System:</b> Console-based banking application in C.</li>
+      <li><b>Movie Review System:</b> Python app with Tkinter and speech recognition.</li>
+    </ul>
+  </section>
+
+  <section id="skills">
+    <h2>Skills</h2>
+    <ul>
+    <p><b>Languages:</b> C, C++, Python, HTML, CSS, JavaScript</p>
+    <p><b>Database:</b> MySQL</p>
+    <p><b>Tools & Platforms:</b> GitHub, GIT, PyCharm, VS Code</p>
+    </ul>
+  </section>
+
+  <section id="certifications">
+    <h2>Certifications</h2>
+    <ul>
+      <li><b>Fundamentals of Generative AI</b> – GUVI (2024)</li>
+      <li><b>Python Matplotlib</b> – Great Learning (2024)</li>
+      <li><b>Pandas</b> – Kaggle (2024)</li>
+      <li><b>The Complete C Development Course</b> – Udemy (2024)</li>
+      <li><b>Mastering Data Structures and Algorithms Using C </b>– Udemy (2024)</li>
+    </ul>
+  </section>
+
+  <section id="achievements">
+    <h2>Achievements</h2>
+    <ul>
+      <li><b>HackerRank:</b> 4-star in Python, 3-star in C++, 2-star in C, 1-star in SQL</li>
+      <li><b>LeetCode:</b> Solved 150+ problems (Rank: 655,845)</li>
+      <li><b>Hackathon: </b>Top 5 finalist in PSG College-level hackathon (2024)</li>
+      <li>Participated in CIT Coimbatore Workshop for Artificial Intelligence</li>
+    </ul>
+  </section>
+
+  <footer>
+    <p>&copy;  Suvethaa DR. </p>
+  </footer>
+</body>
+    )}
+export default Port;
